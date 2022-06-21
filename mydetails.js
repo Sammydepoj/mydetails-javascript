f266d46325f463ad0ef96d9eb536712e5e0df865
Sammydepoj <97622819+Sammydepoj@users.noreply.github.com>
2022-06-22 00:04:49 +0100

    Add files via upload

---
+++ mydetails.js
@@ -0,0 +1,6 @@
+let myname ="Adepoju Adeyemi Samuel";
+let height= 5.85;
+let country = "Nigeria";
+alert("My name is" + " " + myname);
+alert("My height is" + " " + height +" ft ");
+alert("My country is" + " " + country);
